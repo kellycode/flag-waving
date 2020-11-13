da88e1950c48f14fabbb307f72601a9af4d5eb99
kellycode <kwkelly@outlook.com>
2020-11-13 13:56:06 -0600

    details

--- public_html/js/WindMachine.js
+++ public_html/js/WindMachine.js
@@ -1,14 +1,14 @@
 "use strict";
 
-// extracted from three.js examples at https://threejs.org/examples/#webgl_animation_cloth
-
 /*
+ * Based on:
+ * https://threejs.org/examples/#webgl_animation_cloth
  * Cloth Simulation using a relaxed constraints solver
  */
 
 // Suggested Readings
 // Advanced Character Physics by Thomas Jakobsen Character
-// http://freespace.virgin.net/hugo.elias/models/m_cloth.htm
+// https://www.cs.cmu.edu/afs/cs/academic/class/15462-s13/www/lec_slides/Jakobsen.pdf
 // http://en.wikipedia.org/wiki/Cloth_modeling
 // http://cg.alexandra.dk/tag/spring-mass-system/
 // Real-time Cloth Animation http://www.darwin3d.com/gamedev/articles/col0599.pdf
@@ -59,10 +59,14 @@ class Cloth {
         const h = params.ySegs || 10;
         this.w = w;
         this.h = h;
-
-        this.pins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        this.gravity = new THREE.Vector3(0, -params.GRAVITY, 0).multiplyScalar(params.MASS);
+        this.pinsTop = [];
+        this.pinsRight = [];
+        this.pinsBottom = [];
+        this.pinsLeft = [];
         this.diff = new THREE.Vector3();
+
+        // ((x)left/-right, (y)up/-down, (z)front/-back)
+        this.velocity = new THREE.Vector3(params.VELOCITY, 0, 0).multiplyScalar(params.MASS);
 
         this.plane = function (width, height) {
             return function (u, v, target) {
@@ -80,13 +84,42 @@ class Cloth {
         const particles = [];
         const constraints = [];
 
-        // Create particles
+        // Create particles and static sides
         for (let v = 0; v <= h; v++) {
             for (let u = 0; u <= w; u++) {
+
+                if (v === 0) {
+                    this.pinsBottom.push(u);
+                }
+                if (u === w) {
+                    this.pinsRight.push(particles.length);
+                }
+                if (v === h) {
+                    this.pinsTop.push(particles.length);
+                }
+                if (u === 0) {
+                    this.pinsLeft.push(particles.length);
+                }
+
                 particles.push(
                         new Particle(u / w, v / h, 0, params, this)
                         );
             }
+        }
+
+        switch (params.pinSide) {
+            case 'top':
+                this.pins = this.pinsTop;
+                break;
+            case 'right':
+                this.pins = this.pinsRight;
+                break;
+            case 'bottom':
+                this.pins = this.pinsBottom;
+                break;
+            case 'left':
+                this.pins = this.pinsLeft;
+                break;
         }
 
         // Structural
@@ -172,8 +205,8 @@ class Cloth {
 
         for (let i = 0, il = particles.length; i < il; i++) {
             const particle = particles[ i ];
-            particle.addForce(this.gravity);
-            particle.integrate(this.params.TIMESTEP_SQ, this.params);
+            particle.addForce(this.velocity);
+            particle.integrate(this.params.TIMESTEP, this.params);
         }
 
         // Start Constraints
@@ -181,10 +214,8 @@ class Cloth {
         const il = constraints.length;
 
         for (let i = 0; i < il; i++) {
-
             const constraint = constraints[ i ];
             this.satisfyConstraints(constraint[ 0 ], constraint[ 1 ], constraint[ 2 ]);
-
         }
 
         // Floor Constraints
@@ -204,7 +235,6 @@ class Cloth {
             p.previous.copy(p.original);
         }
     }
-
 }
 
 
